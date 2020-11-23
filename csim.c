1a34249df11ebcfe98260ae7c430c770b6e14011
Isaiah Ware <isaiahware99@gmail.com>
2020-11-22 21:27:40 -0600

    asd

--- csim.c
+++ csim.c
@@ -38,48 +38,26 @@ typedef struct line {
 typedef line_t * set_t;
 typedef set_t * cache_t;
 
-unsigned long long int set_index_mask;
+unsigned long long int mask;
 
 cache_t cache;
 
 void initCache() {
-    int i, j;
-    cache = (set_t*)malloc(sizeof(set_t) * S);
-    for (i = 0; i < S; i++) {
-        cache[i] = (line_t*)malloc(sizeof(line_t) * E);
-        for (j = 0; j < E; j++) {
-            cache[i][j].valid = 0;
-            cache[i][j].dirty = 0;
-            cache[i][j].size = 0;
-            cache[i][j].tag = 0;
-            cache[i][j].timestamp = 0;
-        }
-    }
-}
-
-void freeCache() {
-    int i;
-    for (i = 0; i < S; i++) {
-        free(cache[i]);
-    }
-    free(cache);
+    
 }
 
 void store(unsigned long long int addr, int size) {
-    int i;
-    unsigned long long int set_index = (addr >> b) & set_index_mask;
+    unsigned long long int set_index = (addr >> b) & mask;
     unsigned long long int tag = addr >> (s + b);
-    unsigned long long int eviction_lru = ULONG_MAX;
-    unsigned int eviction_line = 0;
 
     set_t set = cache[set_index];
 
-    for (i = 0; i < E; ++i) {
-        if (set[i].valid) {
-            if (set[i].tag == tag) {
-                set[i].timestamp = timestamp++;
-                set[i].dirty = 1;
-                set[i].size = size;
+    for (int line = 0; line < E; ++line) {
+        if (set[line].valid) {
+            if (set[line].tag == tag) {
+                set[line].timestamp = timestamp++;
+                set[line].dirty = 1;
+                set[line].size = size;
                 dirty_active += size;
                 hits++;
                 return;
@@ -87,47 +65,29 @@ void store(unsigned long long int addr, int size) {
         }
     }
 
-    misses++;
-
-    for (int i = 0; i < E; ++i) {
-        if (eviction_lru > set[i].timestamp) {
-            eviction_line = i;
-            eviction_lru = set[i].timestamp;
-        }
-    }
-
-    if (set[eviction_line].valid) {
-        evictions++;
-    }
-
-    if (set[eviction_line].dirty) {
-        dirty_evicted += set[eviction_line].size;
-        dirty_active -= set[eviction_line].size;
-    }
-
-    set[eviction_line].valid = 1;
-    set[eviction_line].tag = tag;
-    set[eviction_line].timestamp = timestamp++;
+    missed(set, tag);
 }
 
 void load(unsigned long long int addr, int size) {
-    int i;
-    unsigned long long int set_index = (addr >> b) & set_index_mask;
+    unsigned long long int set_index = (addr >> b) & mask;
     unsigned long long int tag = addr >> (s + b);
-    unsigned long long int eviction_lru = ULONG_MAX;
-    unsigned int eviction_line = 0;
 
     set_t set = cache[set_index];
 
-    for (i = 0; i < E; ++i) {
-        if (set[i].valid) {
-            if (set[i].tag == tag) {
-                set[i].timestamp = timestamp++;
-                hits++;
-                return;
-            }
+    for (int line = 0; line < E; ++line) {
+        if (set[line].valid == 1 && set[line].tag == tag) {
+            set[line].timestamp = timestamp++;
+            hits++;
+            return;
         }
     }
+
+    missed(set, tag);
+}
+
+void missed(set_t set, unsigned long long int tag) {
+    unsigned long long int eviction_lru = ULONG_MAX;
+    unsigned int eviction_line = 0;
 
     misses++;
 
@@ -147,32 +107,32 @@ void load(unsigned long long int addr, int size) {
         dirty_active -= set[eviction_line].size;
     }
 
-    set[eviction_line].dirty = 0;
     set[eviction_line].valid = 1;
     set[eviction_line].tag = tag;
     set[eviction_line].timestamp = timestamp++;
 }
 
-void replayTrace(char* trace_fn) {
-    FILE* trace_fp = fopen(trace_fn, "r");
-    char trace_cmd;
-    unsigned long long int address;
+void run(char* fileName) {
+    FILE* opened_file = fopen(fileName, "r");
+    char operation;
     int size;
+    unsigned long long int address;
 
-    while (fscanf(trace_fp, " %c %llx,%d", &trace_cmd, &address, &size) == 3) {
-        if (trace_cmd == 'L') {
+
+    while (fscanf(opened_file, " %c %llx,%d", &operation, &address, &size) == 3) {
+        if (operation == 'L') {
             load(address, size);
         }
-        else if (trace_cmd == 'S') {
+        else if (operation == 'S') {
             store(address, size);
         }
-        else if (trace_cmd == 'M') {
+        else if (operation == 'M') {
             load(address, size);
             store(address, size);
         }
     }
 
-    fclose(trace_fp);
+    fclose(opened_file);
 }
 
 int main(int argc, char* argv[]) {
@@ -196,14 +156,32 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    set_index_mask = (unsigned long long int )((1 << s)- 1);
+    mask = (unsigned long long int )((1 << s)- 1);
 
     S = (unsigned int)(1 << s);
     B = (unsigned int)(1 << b);
 
-    initCache();
-    replayTrace(file);
-    freeCache();
+    int i, j;
+    cache = (set_t*)malloc(sizeof(set_t) * S);
+    for (i = 0; i < S; i++) {
+        cache[i] = (line_t*)malloc(sizeof(line_t) * E);
+        for (j = 0; j < E; j++) {
+            cache[i][j].valid = 0;
+            cache[i][j].dirty = 0;
+            cache[i][j].size = 0;
+            cache[i][j].tag = 0;
+            cache[i][j].timestamp = 0;
+        }
+    }
+
+    run(file);
+
+    int i;
+    for (i = 0; i < S; i++) {
+        free(cache[i]);
+    }
+    free(cache);
+
     printSummary(hits, misses, evictions, dirty_evicted, dirty_active, double_accesses);
 
     return 0;
