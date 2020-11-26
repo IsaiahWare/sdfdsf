ce28b5bf3179c3d84ebf56999e23501d37d9ee10
Isaiah Ware <isaiahware99@gmail.com>
2020-11-25 18:36:20 -0600

    asd

--- trans.c
+++ trans.c
@@ -1,4 +1,7 @@
-/* Philip J. Ottesen - NYUID: pjo256 
+/* 
+ * Name : Enze Li
+ * Andrew id : enzel
+ *
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -9,6 +12,7 @@
  */ 
 #include <stdio.h>
 #include "cachelab.h"
+#include "contracts.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
@@ -17,137 +21,135 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded. The REQUIRES and ENSURES from 15-122 are included
+ *     for your convenience. They can be removed if you like.
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-    /*
-    if (M == N) {
-        trans_square(M, N, A, B);
-    } else {
-        trans_diff(M, N, A, B);
-    }
-    */
-    int i, j, row_block, col_block;
-    int diag = 0;
-    int temp = 0;
+    int blocksize;
+    int row, col, i, j;
+    int a, b, x, y, z; 
 
-    /*
-    The access pattern for the defined problem sizes incorporate blocking; we define a sub-matrix of the matrix A with so    me size b to be a square block. The outer-loops iterate across these block structures, with the two inner loops          iterating through each block.
-    */
+    REQUIRES(M > 0);
+    REQUIRES(N > 0);
 
-    if (N == 32) 
-    {
-        //Finding optimal block sizes included some guess-work; increased B-size for larger M & N when M == N proved less        //efficient. However, when M != N, i.e. rectangular matrix, larger B was the way to go.
-        for (col_block = 0; col_block < N; col_block += 8) 
-        {
-            for (row_block = 0; row_block < N; row_block += 8) 
-            {
-            
-                for (i = row_block; i < row_block + 8; i ++) 
-                {
-
-                    for (j = col_block; j < col_block + 8; j ++) 
-                    {
-                        if (i != j) 
-                        {
-                            B[j][i] = A[i][j];
-                        }
-                         else {
-                            //Reduce misses m < i*j in B by storing in temp instead of missing in B[j][i]
-                            temp = A[i][j];
-                            diag = i;
-                        }
+    /* save diagnal to last, avoid cache confliction of Aii and Bii */
+    if (M == 32 && N == 32) {
+        blocksize = 8;
+        for (row = 0; row < N; row += blocksize){
+            for (col = 0; col < M; col += blocksize){
+                for (i = row; i < row + blocksize && i < N; ++i){
+                    for (j = col; j < col + blocksize && j < M; ++j){
+                        if (i != j) {
+                            a = A[i][j];
+                            B[j][i] = a;
+                        } 
                     }
-                    
-                    //Transpose of a square-matrix has a unique property; no need to move elements on the diagonal. 
-                
-                    if (row_block == col_block) 
-                    {
-                        //Misses in B reduced to m < i
-                        B[diag][diag] = temp;
+                    if (row == col) {
+                        a = A[i][i];
+                        B[i][i] = a;
                     }
-                }   
-                
-            }
-        }
-
-    } 
-
-    else if (N == 64) 
-    {
-
-        //Iterate through matrix using column-major iteration over blocks
-        for (col_block = 0; col_block < N; col_block += 4) 
-        {
-            for (row_block = 0; row_block < N; row_block += 4) 
-            {
-                //Iterate over each row using row-major iteration           
-                for (i = row_block; i < row_block + 4; i ++) 
-                {
-                    for (j = col_block; j < col_block + 4; j ++) 
-                    {
-                        if (i != j) 
-                        {
-                            B[j][i] = A[i][j];
-                        }
-                        else {
-                            //On the diagonal
-                            temp = A[i][j];
-                            diag = i;
-                        }
-                    }
-
-                    if (row_block == col_block) 
-                    {
-                        B[diag][diag] = temp;
-                    }
-                }   
-                
-            }
-        }
-        
-
-    } 
-    else {
-        
-        //Iterate through matrix using column-major iteration over blocks
-        for (col_block = 0; col_block < M; col_block += 16) 
-        {
-            for (row_block = 0; row_block < N; row_block += 16) 
-            {       
-                //Since our sizes are prime, not all blocks will be square sub-matrices
-                //Consider corner-case when (row_block + 16 > N) => invalid access. Explicit check for i, j < n, m
-                for (i = row_block; (i < row_block + 16) && (i < N); i ++) 
-                {
-                    for (j = col_block; (j < col_block + 16) && (j < M); j ++) 
-                    {
-                    
-                        if (i != j) 
-                        {
-                            B[j][i] = A[i][j];
-                        }
-                        else 
-                        {
-                            temp = A[i][j];
-                            diag = i;
-                        }
-                    }
-
-                    if (row_block == col_block) {
-                        B[diag][diag] = temp;
-                    }
-
                 }
-            
             }
         }
-    
     }
 
+    /* 
+     *  4X4 blocks 
+     *  if block is near diagnal, A[i][j] and B[j][i] cached in same set 
+     *  try to use every item before one is evicted by another
+     */
+
+    if (M == 64 && N == 64) {
+        blocksize = 4;
+        for (i = 0; i < N; i += blocksize){
+            for (j = 0; j < M; j += blocksize){
+                /* load A[i][] A[i+1][] A[i+2][]*/
+                a = A[i][j];
+                b = A[i+1][j];
+                x = A[i+2][j];
+                y = A[i+2][j+1];
+                z = A[i+2][j+2];
+
+                /* load B[j+3][] */
+                B[j+3][i] = A[i][j+3];
+                B[j+3][i+1] = A[i+1][j+3];
+                B[j+3][i+2] = A[i+2][j+3];
+
+                /* load B[j+2][], may evict A[i+2][] */
+                B[j+2][i] = A[i][j+2];
+                B[j+2][i+1] = A[i+1][j+2];
+                B[j+2][i+2] = z;
+
+                z = A[i+1][j+1];
+
+                /* load B[j+1][], may evict A[i+1][] */
+                B[j+1][i] = A[i][j+1];
+                B[j+1][i+1] = z;
+                B[j+1][i+2] = y;
+
+                /* load B[j][], may evict A[i][] */
+                B[j][i] = a;
+                B[j][i+1] = b;
+                B[j][i+2] = x;
+
+                /* load A[i+3][], may evict B[j+3][] */
+                B[j][i+3] = A[i+3][j];
+                B[j+1][i+3] = A[i+3][j+1];
+                B[j+2][i+3] = A[i+3][j+2];
+
+                a = A[i+3][j+3];
+
+                /* load B[j+3][], may evict A[i+3][] */
+                B[j+3][i+3] = a;
+            }
+        }
+    }
+
+    /* a proper blocksize will work */
+    if (M == 61 && N == 67) {
+        blocksize = 18;
+        for (row = 0; row < N; row += blocksize){
+            for (col = 0; col < M; col += blocksize){
+                for (i = row; i < row + blocksize && i < N; ++i){
+                    for (j = col; j < col + blocksize && j < M; ++j){
+                        a = A[i][j];
+                        B[j][i] = a;
+                    }
+                }
+            }
+        }
+    }
+
+    ENSURES(is_transpose(M, N, A, B));
 }
 
+/* 
+ * You can define additional transpose functions below. We've defined
+ * a simple one below to help you get started. 
+ */ 
+
+/* 
+ * trans - A simple baseline transpose function, not optimized for the cache.
+ */
+char trans_desc[] = "Simple row-wise scan transpose";
+void trans(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tmp;
+
+    REQUIRES(M > 0);
+    REQUIRES(N > 0);
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }    
+
+    ENSURES(is_transpose(M, N, A, B));
+}
 
 /*
  * registerFunctions - This function registers your transpose
@@ -162,6 +164,7 @@ void registerFunctions()
     registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
+    registerTransFunction(trans, trans_desc); 
 
 }
 
@@ -173,7 +176,6 @@ void registerFunctions()
 int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
-
     for (i = 0; i < N; i++) {
         for (j = 0; j < M; ++j) {
             if (A[i][j] != B[j][i]) {
